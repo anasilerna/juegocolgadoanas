9f3a17f3f652ed547afbc0f1e606d2353267b853
anasilerna <anasmejahad15@alumnes.ilerna.com>
2024-12-01 13:49:26 +0100

    // Preguntar cuántas rondas jugarán

--- src/juegocolgadoanas/juegocolgadoanas.java
+++ src/juegocolgadoanas/juegocolgadoanas.java
@@ -7,6 +7,12 @@ public class juegocolgadoanas {
 		 Scanner scanner = new Scanner(System.in);
 		 // Iniciar el juego
 	        System.out.println("Bienvenidos al Juego del Colgado");
+	        
+	        // Preguntar cuántas rondas jugarán
+	        System.out.print("¿Cuántas rondas quieren jugar?: ");
+	        int numRondas = scanner.nextInt();
+	        scanner.nextLine(); 
+
 	}
 
 }

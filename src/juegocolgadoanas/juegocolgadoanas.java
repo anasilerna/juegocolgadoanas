8ebbd5f3a41832e1c51ced796e709e06362c111d
anasilerna <anasmejahad15@alumnes.ilerna.com>
2024-12-01 13:45:56 +0100

    scanner

--- src/juegocolgadoanas/juegocolgadoanas.java
+++ src/juegocolgadoanas/juegocolgadoanas.java
@@ -1,10 +1,10 @@
 package juegocolgadoanas;
-
+import java.util.Scanner;
 public class juegocolgadoanas {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		 Scanner scanner = new Scanner(System.in);
 	}
 
 }
